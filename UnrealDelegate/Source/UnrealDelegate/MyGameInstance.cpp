7524d2be7bcdf3361807b5b01fea2eff854d656b
SEOK IN GYEONG <106717276+InGyeongSeok@users.noreply.github.com>
2024-01-27 23:44:42 +0900

    09. 언리얼 C++ 설계3 - 델리게이트

---
+++ UnrealDelegate/Source/UnrealDelegate/MyGameInstance.cpp
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyGameInstance.h"
+#include "Student.h"
+#include "Teacher.h"
+#include "Staff.h"
+#include "Card.h"
+
+UMyGameInstance::UMyGameInstance()
+{
+	SchoolName = TEXT("기본학교"); //이 기본값은 Class Default Object라는 템플릿 객체에 저장이 된 채로 존재
+}
+
+void UMyGameInstance::Init()
+{
+	Super::Init();
+
+	UE_LOG(LogTemp, Log, TEXT("============================"));
+
+	TArray<UPerson*> Persons = { NewObject<UStudent>(), NewObject<UTeacher>(), NewObject<UStaff>() };
+	for (const auto Person : Persons)
+	{
+		const UCard* OwnCard = Person->GetCard();
+		check(OwnCard);
+		ECardType CardType = OwnCard->GetCardType();
+		UE_LOG(LogTemp, Log, TEXT("%s님이 소유한 카드의 종류는 %d"), *Person->GetName(), CardType);
+
+		const UEnum* CardEnumType = FindObject<UEnum>(nullptr, TEXT("/Script/UnrealComposition.ECardType")); //TEXT에 들어가는 절대 주소값을 사용해서 원하는 타입 정보를 얻어올 수 있다.
+		//C++에 생성된 언리얼 객체는 Script라는 절대 주소를 가진다 // 모듈(UnrealComposition)에서 타입을 가지고 온다 
+		if (CardEnumType)
+		{
+			FString CardMetaData = CardEnumType->GetDisplayNameTextByValue((int64)CardType).ToString();
+			UE_LOG(LogTemp, Log, TEXT("%s님이 소유한 카드의 종류는 %s"), *Person->GetName(), *CardMetaData);
+		}
+	}
+
+	UE_LOG(LogTemp, Log, TEXT("============================"));
+}
---
+++ UnrealDelegate/Source/UnrealDelegate/Person.cpp
@@ -0,0 +1,12 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Person.h"
+#include "Card.h"
+
+UPerson::UPerson()
+{
+	Name = TEXT("홍길동");
+	Card = CreateDefaultSubobject<UCard>(TEXT("NAME_Card")); //첫번째 인자는 FName이라는 식별자를 넣어줘야 한다 ->고유한 이름이면 된다
+	//앞에 Name_ 이라고 하면 FName이라는 것을 알 수 있다.(String이 아니라!)
+}	
